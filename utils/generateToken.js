62dde784051b5b6c15f06700ae4da1f2cb4dd897
itzSerag <serag.eldien.mahmoud@gmail.com>
2024-03-08 13:39:37 +0200

    Revert "DocTalker__Backend"

--- utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-
-exports.generateToken = (data)=>{
-    const token = jwt.sign(data,process.env.JWT_SECRET_KEY,{
-        expiresIn:process.env.JWT_EXPIRE_TIME
-    })
-    return token
-}
