1240b288c31690f56f40f648c7c745a04b8035bc
Peter Khalil <peter.khalil.fahmy@gmail.com>
2024-03-14 05:54:00 +0200

    Creating a middleware that handles the subscription plans

--- routes/upload.js
+++ routes/upload.js
@@ -3,14 +3,32 @@ const { fileUpload, folderUpload } = require('../controllers/uploadController');
 const processController = require('../controllers/processController');
 const { upload } = require('../utils/uploadFile');
 const { auth } = require('../middlewares/auth');
+const {
+  checkUploadRequest,
+  checkFileType,
+} = require('../middlewares/isAuthorized');
 
 const router = express.Router();
 
 // Handle single file upload
-router.post('/upload', auth, upload.single('file'), fileUpload);
+router.post(
+  '/upload',
+  auth,
+  upload.single('file'),
+  checkUploadRequest,
+  checkFileType,
+  fileUpload
+);
 
 // Handle folder upload
-router.post('/uploadfolder', auth, upload.array('files'), folderUpload);
+router.post(
+  '/uploadfolder',
+  auth,
+  upload.array('files'),
+  checkUploadRequest,
+  checkFileType,
+  folderUpload
+);
 
 // Handle processing
 router.post('/process', auth, processController.handler);
