ba1c5063fd562e1c27d5c488969c54b31831719a
itzSerag <serag.eldien.mahmoud@gmail.com>
2024-06-06 19:04:35 +0300

    conn

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,10 +6,10 @@ exports.auth = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        req.user = await user.findById(decoded._id); // already attached to the request object -- mongoose model
+        const user = await User.findById(decoded._id); // already attached to the request object -- mongoose model
 
-        if (req.user === null) {
-            return res.status(401).json({ message: 'Unauthorized' });
+        if (user === null) {
+           next(new AppError('User not Authorized', 404));   
         }
         req.user = user;
         next();
